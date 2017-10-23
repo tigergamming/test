05b970724ec20f6067e6434f5c5615547ad85321
ProMinerOfMC <ProMinerOfMC@users.noreply.github.com>
2017-10-23 09:57:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,5 +17,17 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', function(message) {
+    if (message.content == "^clear") {
+        if (message.member.hasPermission("MANAGE_MESSAGES")) {
+            message.channel.fetchMessages()
+               .then(function(list){
+                    message.channel.bulkDelete(list);
+                }, function(err){message.channel.send("ERROR: ERROR CLEARING CHANNEL.")})                        
+        }
+    }
+
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
