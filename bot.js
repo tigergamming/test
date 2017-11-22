117def643cfaf99fe7c84282a74fcc3eeb98a63f
Josh <19347018+DefinitelyNotJosh@users.noreply.github.com>
2017-11-22 13:30:46 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ const config = require("./config.json");
 
 let prefix = config.prefix;
 
-client.on("ready", function() {
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setGame(prefix + "help");
+client.on("ready", () => {
+    console.log('Ready to begin! Serving in ' + bot.guilds.array().length + ' servers.');
+    bot.user.setStatus("online", "");
 });
 
 client.on("guildCreate", guild => {
@@ -94,37 +94,9 @@ client.on('message', message => {
   
     if (command === "invite") {
         
-        message.channel.send("You can add JoshBot to your server here: https://discordapp.com/oauth2/authorize?client_id=371553407569362955&scope=bot&permissions=8");
+        message.channel.send("You can add JoshBot to your server here: https://discordapp.com/oauth2/authorize?client_id=371553407569362955&scope=bot&permissions=
         
     }
-  
-      if(command === "kick") {
-    // This command must be limited to mods and admins. In this example we just hardcode the role names.
-    // Please read on Array.some() to understand this bit: 
-    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
-      return message.reply("Sorry, you don't have permissions to use this!");
-    
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    let member = message.mentions.members.first();
-    if(!member)
-      return message.reply("Please mention a valid member of this server");
-    if(!member.kickable) 
-      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
-    
-    // slice(1) removes the first part, which here should be the user mention!
-    let reason = args.slice(1).join(' ');
-    if(!reason)
-      return message.reply("Please indicate a reason for the kick!");
-    
-    // Now, time for a swift kick in the nuts!
-    await member.kick(reason)
-      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
-    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
-
-    }
-
   
     let mess = message.content.split(" ").slice(1);
     
