932b41aae2b08a3c8c93444cf998796bf206d2e4
Josh <19347018+DefinitelyNotJosh@users.noreply.github.com>
2017-11-23 16:14:51 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,10 @@ const config = require("./config.json");
 
 let prefix = config.prefix;
 
+client.on('ready', () => {
+client.user.setUsername("OnO")
+});
+
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
