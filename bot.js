ec85723d332b1678fca472017ec004769366a50f
LightDarkness <trumselot123@gmail.com>
2017-11-25 02:37:53 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -89,7 +89,7 @@ client.on('message', message => {
         
         message.channel.send('Pong...').then(msg => {
             
-            msg.edit(`**Pong**! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms.`);
+            msg.edit(`**Pong**! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms.`)});
  
     }
                                              
