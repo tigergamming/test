19c5ec25239d5864ce9b3014e986de45095d770f
Josh <19347018+DefinitelyNotJosh@users.noreply.github.com>
2017-12-05 07:13:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
     // This will trigger when the bot comes online.
     console.log(`${client.user.tag} Is Active!`);
     console.log(`----------------`);
-    client.user.setPresence({game: {name: `?help | on ${client.guilds.size} servers`, type: 0}});
+    client.user.setPresence({game: {name: `on ${client.guilds.size} servers | ?help`, type: 0}});
 });
 
 client.on("guildCreate", guild => {
